72b7c207be82dea4ce7ea6debf188fdc0ef59d39
DrummyG <balestrinodavide@gmail.com>
2022-04-20 13:34:54 +0200

    esercizio completo

---
+++ script.js
@@ -0,0 +1,8 @@
+const root = new Vue({
+    el: '#app',
+    data:{
+        testo: 'Hello world',
+        image: 'https://www.lenny76.com/wp-content/uploads/2018/03/hello-world.jpg',
+        saved: 'HelloWorld.png'
+    },
+})
